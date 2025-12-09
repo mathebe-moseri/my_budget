1eb44c42183895781e6ddf2a9052e6f817241be4
mathebe-moseri <mathebemoseri@gmail.com>
2025-12-09 14:11:30 +0200

    chore: define routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,26 @@
 import { Routes } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BudgetDetailsComponent } from './pages/budget-details/budget-details.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
 
 export const routes: Routes = [
-    {path: '', component: NavComponent}
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+        {
+        path: 'details/id',
+        component: BudgetDetailsComponent
+    },
+
+        {
+        path: 'create-account',
+        component: CreateAccountComponent
+    },
 ];
